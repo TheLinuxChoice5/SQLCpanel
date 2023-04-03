c74ab7d7664ce8bd3640e6863c63a1705d36e3a9
TheLinuxChoice5 <125088134+TheLinuxChoice5@users.noreply.github.com>
2023-04-03 10:08:17 +0200

    Update login.php
    boostrap and dark mode implementation

--- login.php
+++ login.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<title>Login Page</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark text-white">
 	<h1>Login Page</h1>
 	
 	<form action="panel.php" method="post">
